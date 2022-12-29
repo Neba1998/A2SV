f54c92c631a9e0f13b4b9bd32cd3a0b5d7052fab
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2022-12-29 22:45:44 +0300

    Create Fibonacci Number.js

---
+++ Fibonacci Number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+  if(n==1){
+    return 1;
+  } else if(n==0){
+     return 0;
+  }else if(n>1){
+    return fib(n-1)+fib(n-2);
+  }
+};
