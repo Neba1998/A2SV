add6fc90b6f47de3df82279606f7bf02a62c4317
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2022-12-30 23:23:59 +0300

    Create Find Pivot Index.js

---
+++ Find Pivot Index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+   let totalSum = 0;
+   for(let i = 0;i <nums.length; i++){
+       totalSum+=nums[i];
+   }
+   let leftSum=0;
+   for(let i = 0;i < nums.length;i++){
+       if(leftSum==totalSum-leftSum-nums[i]){
+           return i;
+       }
+       leftSum+=nums[i];
+   }
+   return -1;
+};
