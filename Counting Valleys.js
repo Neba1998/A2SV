2e3779dfe8f1e11d662f737631ca6a1060d5813a
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2022-12-31 12:38:46 +0300

    Create Counting Valleys.js

---
+++ Counting Valleys.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+    let valley=0;
+    let elivation=0;
+    for(let i =0;i<steps;i++){
+        if(path[i]== "D"){
+            elivation--;
+        }else{
+           if (elivation== -1){
+               valley++;
+           }
+           elivation++;
+        }
+    }
+  return valley;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
