9cf146244b11feaf4c0d65008b7969065bfb6133
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2023-01-08 21:14:17 +0300

    Create Next Greater Element I.js

---
+++ Next Greater Element I.js
@@ -0,0 +1,21 @@
+ const hashmap = {};
+    const output = [];
+    
+    for (let i = 0; i < nums2.length; i++) {
+        hashmap[nums2[i]] = i;
+    }
+    
+    for (let i = 0; i < nums1.length; i++) {
+        const number = nums1[i];
+        for (let j = hashmap[number]; j < nums2.length; j++) {
+            if (nums2[j] > number) {
+                output.push(nums2[j]);
+                break;
+            }
+            if (j === nums2.length - 1) {
+                output.push(-1);
+            }
+        }
+    }
+    
+    return output;
