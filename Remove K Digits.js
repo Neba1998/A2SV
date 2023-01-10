0b3fadcde458aac785dbce10fff33ee84d592497
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2023-01-10 20:48:31 +0300

    Create  Remove K Digits.js

---
+++ Remove K Digits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+     const stack = [];
+    let removed = 0;
+    for(let n of num) {
+        while(stack.length && n < stack[stack.length-1] && removed < k) {
+            stack.pop();
+            removed += 1;
+        }
+        stack.push(n);
+    }
+    while(removed < k) {
+        stack.pop();
+        removed += 1;
+    }
+    while(stack.length && stack[0] === '0') {
+        stack.shift();
+    }
+    
+    return stack.length ? stack.join('') : '0';
+};
