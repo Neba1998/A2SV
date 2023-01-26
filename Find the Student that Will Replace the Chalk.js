71a4976a76c120a698ceb55860393c6a0f8c5b59
Neba1998 <112127459+Neba1998@users.noreply.github.com>
2023-01-26 21:11:56 +0300

    Create Find the Student that Will Replace the Chalk.js

---
+++ Find the Student that Will Replace the Chalk.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} chalk
+ * @param {number} k
+ * @return {number}
+ */
+var chalkReplacer = function(chalk, k) {
+      const sum = chalk.reduce((r, c) => r + c, 0);
+    
+    k %= sum;
+    
+    for (let i = 0; i < chalk.length; i++) {
+        if (chalk[i] > k) {
+            return i;
+        }
+
+        k -= chalk[i];
+    }
+};
